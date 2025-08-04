6c94b94469ba50477e8117315ab77024d5e0a41d
DomTheFlom1 <dominik.ries@easytux.ch>
2025-08-04 15:35:20 +0200

    ...

---
+++ Start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "1) AutoUpdater"
+echo "2) Server Creator"
+echo "3) Server Updater"
+
+read -p "What do you want to start?  " r
+
+case $r in
+
+    1) bash autoupdate.sh;;
+    2) bash new.sh;;
+    3) bash update.sh;;
+    *) echo Wot? && bash Start.sh;;
+
+esac
+
+
